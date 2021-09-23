b4196f9056e5ba2bc434fa60dcc163152e2e0aed
ariellezcano <ariellezcano1995@gmail.com>
2021-09-23 08:50:41 -0300

    creado lst-marca

--- Deposito/src/app/pages/frm-abm/abm-marca/abm-marca.component.ts
+++ Deposito/src/app/pages/frm-abm/abm-marca/abm-marca.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Marca } from 'src/app/modelo/index.models';
 import { MarcaService } from 'src/app/servicio/index.service';
@@ -11,15 +11,26 @@ import Swal from 'sweetalert2';
 })
 export class AbmMarcaComponent implements OnInit {
 
+  @Output()
+  finalizado = new EventEmitter<Boolean>();
+  
   item!: Marca;
+  form!: NgForm;
 
-  constructor(private wsdl: MarcaService) { }
+  constructor(private wsdl: MarcaService) { 
+    this.item = new Marca();
+  }
 
   ngOnInit(): void {
   }
 
   accion(f:NgForm){
-    if (this.item.id>0){
+    if (f.invalid) {
+      this.form.ngSubmit;
+      //alert("asa");
+      return;
+    }
+    if (this.item.id > 0){
       this.editar();
     }else{
       this.crear();
@@ -31,11 +42,13 @@ async editar(){
 
 }
 async crear(){
-  const data = await this.wsdl.doInsert(this.item).then();
-      const res = JSON.parse(JSON.stringify(data));
-      if (res.status === 200) {
+  this.finalizado.emit(true);
+  console.log(this.item)
+  // const data = await this.wsdl.doInsert(this.item).then();
+  //     const res = JSON.parse(JSON.stringify(data));
+  //     if (res.status === 200) {
         
-      }
+  //     }
 }
 
 }
--- Deposito/src/app/pages/lst/lst-marca/lst-marca.component.ts
+++ Deposito/src/app/pages/lst/lst-marca/lst-marca.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Marca } from 'src/app/modelo/index.models';
 
 @Component({
   selector: 'app-lst-marca',
   templateUrl: './lst-marca.component.html',
-  styleUrls: ['./lst-marca.component.scss']
+  styleUrls: ['./lst-marca.component.scss'],
 })
 export class LstMarcaComponent implements OnInit {
-entity = "Listado de marcas";
-id = 1;
-marca = "HP";
-  constructor() { }
+  entity = 'Listado de marcas';
+  id = 1;
+  marca = 'HP';
 
-  ngOnInit(): void {
+  item!: Marca;
+  items!: Marca[];
+
+  @ViewChild('close')
+  cerrar!: ElementRef;
+
+
+  //evento que envia los datos del abm al listado
+  evento(event: Boolean) {
+    console.log('se creo correctamente', this.item);
+    this.cerrar.nativeElement.click();
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
