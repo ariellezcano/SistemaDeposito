082a2d9901ca81a232de107650df111de81e1ac2
ariellezcano <ariellezcano1995@gmail.com>
2021-10-26 09:16:13 -0300

    seguimos

--- Deposito/src/environments/environment.ts
+++ Deposito/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   //prueba
   URL: "http://10.125.31.241:3001/",
   //local
-  //URL: "http://10.125.31.150:3001/",
+  // URL: "http://10.125.31.150:3001/",
   URLPOLD: "http://10.125.31.150:3000/",
 
 };
